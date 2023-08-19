07a32ef56797eb77240c077d076b08b4a985de72
wave <122275075+ww20hust@users.noreply.github.com>
2023-08-19 20:43:58 +0800

    Update PCA.r

--- PCA.r
+++ PCA.r
@@ -1,1 +1,21 @@
+#####PCA#####
+library(ggpubr)
+pca.info <- prcomp(exprset.symbol)
+summary(pca.info)
+pca.data <- data.frame(sample = rownames(pca.info$rotation),
+                       Type = colnames(exprset.symbol),
+                       pca.info$rotation)
+## 绘制PCA散点图
+ggscatter(pca.data,x = "PC1",y = "PC2",
+          color = "Type",
+          size = 2
+)+
+  theme_bw()+
+  ggtitle("PAC分析")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+#####hclust聚类#####
+exprset.symbol %>% t() %>% 
+  dist() %>% hclust() %>% 
+  plclust()/plot()
 
